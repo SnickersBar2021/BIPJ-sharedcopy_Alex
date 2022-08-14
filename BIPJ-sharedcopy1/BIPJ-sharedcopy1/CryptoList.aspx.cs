26bf979f36d0beb37d1100dd2fbbb8e785cb7653
SnickersBar2021 <88878596+SnickersBar2021@users.noreply.github.com>
2022-08-14 21:07:39 +0800

    BIPJ Integrated (Alex)

---
+++ BIPJ-sharedcopy1/BIPJ-sharedcopy1/CryptoList.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BIPJ_sharedcopy1
+{
+    public partial class CryptoList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DataList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
